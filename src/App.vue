97b9a8a86bb3543bc8a07d3fa47b962b83b441a1
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-08-14 14:11:45 +0200

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,31 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+<!--      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>-->
       <v-toolbar-title>AppName</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn small color="primary" @click="historyToClipboard()">Export Probabilistic History</v-btn>
-      <v-snackbar v-model="snackbar">
-        History copied to clipboard.
-        <template v-slot:action="{ attrs }">
-          <v-btn
-              color="pink"
-              text
-              v-bind="attrs"
-              @click="snackbar = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list nav dense>
-        <v-list-item-group
-          active-class="primary--text text--accent-4"
-        >
-          <v-list-item
-            v-for="route in routes" :key="route.name" :to="{path: route.path}">
-            <v-list-item-icon>
-              <v-icon>{{ route.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ route.name }}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+<!--    <v-navigation-drawer-->
+<!--      v-model="drawer"-->
+<!--      absolute-->
+<!--      temporary-->
+<!--    >-->
+<!--      <v-list nav dense>-->
+<!--        <v-list-item-group-->
+<!--          active-class="primary&#45;&#45;text text&#45;&#45;accent-4"-->
+<!--        >-->
+<!--          <v-list-item-->
+<!--            v-for="route in routes" :key="route.name" :to="{path: route.path}">-->
+<!--            <v-list-item-icon>-->
+<!--              <v-icon>{{ route.icon }}</v-icon>-->
+<!--            </v-list-item-icon>-->
+<!--            <v-list-item-title>{{ route.name }}</v-list-item-title>-->
+<!--          </v-list-item>-->
+<!--        </v-list-item-group>-->
+<!--      </v-list>-->
+<!--    </v-navigation-drawer>-->
 
-    <v-main class="grey darken-2">
+    <v-main class="primary">
       <v-container fluid>
         <router-view></router-view>
       </v-container>
--- src/views/Model.vue
+++ src/views/Model.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="model">
     <v-row class="container-sticky">
-      <v-col cols="12" sm="12" lg="6">
-        <history />
+      <v-col cols="12" sm="12" lg="5">
+        <v-card class="mx-auto">
+          <v-tabs grow v-model="tab" x-small>
+            <v-tab href="#tab1" class="text-overline pa-0">History</v-tab>
+            <v-tab href="#tab2" class="text-overline pa-0">Rocks</v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="tab">
+            <v-tab-item :key="1" value="tab1">
+              <History />
+            </v-tab-item>
+            <v-tab-item :key="2" value="tab2">
+              <RockLibrary />
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
       </v-col>
       <v-col class="preview-sticky">
         <preview class="preview-sticky"/>
@@ -15,14 +28,20 @@
 
 import Preview from '@/components/Preview.vue'
 import History from '@/components/History.vue'
+import RockLibrary from "@/views/RockLibrary";
 // import Convolution from "../components/Convolution";
 
 export default {
   name: 'Model',
   components: {
-    // Convolution,
+    RockLibrary,
     Preview,
     History
+  },
+  data() {
+    return {
+      tab: 'tab1'
+    }
   }
 }
 </script>
