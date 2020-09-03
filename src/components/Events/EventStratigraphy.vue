43a7e9d4ba9aa49e955c554c09ce44d36c2ad952
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-09-03 12:29:28 +0200

    Removes manual stratigraphy edit

--- src/components/Events/EventStratigraphy.vue
+++ src/components/Events/EventStratigraphy.vue
@@ -1,25 +1,26 @@
 <template>
 <div class="mx-0">
-  <v-tabs grow v-model="tab" x-small class="mb-10">
-    <v-tab href="#tab1" class="text-overline pa-0">Random Generator</v-tab>
-    <v-tab href="#tab2" class="text-overline pa-0">Manual</v-tab>
-  </v-tabs>
-  <v-tabs-items v-model="tab">
-    <v-tab-item :key="1" value="tab1">
-      <v-card flat>
-        <RandomStratigraphyGenerator :eventIndex="0" />
-      </v-card>
-    </v-tab-item>
-    <v-tab-item :key="2" value="tab2">
-      <StratigraphyEdit :eventIndex="eventIndex" />
-    </v-tab-item>
-  </v-tabs-items>
+  <RandomStratigraphyGenerator :eventIndex="0" />
+<!--  <v-tabs grow v-model="tab" x-small class="mb-10">-->
+<!--    <v-tab href="#tab1" class="text-overline pa-0">Random Generator</v-tab>-->
+<!--    <v-tab href="#tab2" class="text-overline pa-0">Manual</v-tab>-->
+<!--  </v-tabs>-->
+<!--  <v-tabs-items v-model="tab">-->
+<!--    <v-tab-item :key="1" value="tab1">-->
+<!--      <v-card flat>-->
+<!--        <RandomStratigraphyGenerator :eventIndex="0" />-->
+<!--      </v-card>-->
+<!--    </v-tab-item>-->
+<!--    <v-tab-item :key="2" value="tab2">-->
+<!--      <StratigraphyEdit :eventIndex="eventIndex" />-->
+<!--    </v-tab-item>-->
+<!--  </v-tabs-items>-->
 </div>
 
 </template>
 
 <script>
-import StratigraphyEdit from '@/components/Events/StratigraphyEdit.vue'
+// import StratigraphyEdit from '@/components/Events/StratigraphyEdit.vue'
 import RandomStratigraphyGenerator from "../RandomStratigraphyGenerator";
 
 export default {
@@ -27,7 +28,7 @@ export default {
   props: ['eventIndex'],
   components: {
       RandomStratigraphyGenerator,
-      StratigraphyEdit
+      // StratigraphyEdit
   },
   computed: {
     event: function () {
